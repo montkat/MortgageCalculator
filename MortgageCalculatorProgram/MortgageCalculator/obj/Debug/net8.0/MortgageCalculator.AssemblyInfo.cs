96623ebc26a3e23fe7a3130e7abf3cecd8fd419b
montkat <175044201+montkat@users.noreply.github.com>
2024-08-12 16:34:23 -0500

    Changes

--- MortgageCalculatorProgram/MortgageCalculator/obj/Debug/net8.0/MortgageCalculator.AssemblyInfo.cs
+++ MortgageCalculatorProgram/MortgageCalculator/obj/Debug/net8.0/MortgageCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MortgageCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae156762dec747984d611c77bdb12d95ee880bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68a9d6826376ea590575fb25e1982e5d2f121471")]
 [assembly: System.Reflection.AssemblyProductAttribute("MortgageCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MortgageCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
